86ed61de122a36731ada7393ec3139ba94973a0a
yacineb17120 <yacinefadlallah.bestaoui@univ-tlemcen.dz>
2021-03-31 18:21:53 +0100

    Create Domain(Ingredient) and adding OneToMany and ManyToOne for Ingredient and Recipe relationship.

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,54 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String description;
+    private BigDecimal amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    //private UnitOfMeasure unitOfMeasure;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
