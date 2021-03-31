d05be29d97d71b28c7b10595b0c675d90ee57b82
yacineb17120 <yacinefadlallah.bestaoui@univ-tlemcen.dz>
2021-03-31 17:57:19 +0100

    Create Domain(Notes, Recipe) and adding OneToOne relationship.

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String notes;
+
+    @OneToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
